fedd997341ee3d00042ca760ed8c3e2d25b9da07
Wisdom Ogwu <iammadab@gmail.com>
2024-12-03 20:50:36 +0100

    decoding v1

--- src/decode_instruction.rs
+++ src/decode_instruction.rs
@@ -93,7 +93,7 @@ pub(crate) fn decode_instruction(instruction: u32) -> DecodedInstruction {
     let imm = decode_immediate(&inst_type, instruction);
 
     DecodedInstruction {
-        opcode: decode_opcode(opcode_value, &inst_type, funct3, funct7),
+        opcode: decode_opcode(opcode_value, &inst_type, funct3, funct7, imm),
         inst_type,
         rd,
         rs1,
@@ -109,6 +109,7 @@ fn decode_opcode(
     inst_type: &InstructionType,
     funct3: u32,
     funct7: u32,
+    imm: u32,
 ) -> Opcode {
     match inst_type {
         InstructionType::R => match funct3 {
@@ -133,22 +134,21 @@ fn decode_opcode(
         InstructionType::I => {
             match opcode_value {
                 // alu
-                0b0010011 => {
-                    match funct3 {
-                        0x0 => Opcode::Addi,
-                        0x4 => Opcode::Xori,
-                        0x6 => Opcode::Ori,
-                        0x7 => Opcode::Andi,
-                        0x1 => Opcode::Slli,
-                        0x5 => {
-                            // not implemented because it requires an immediate value check
-                            todo!()
-                        }
-                        0x2 => Opcode::Slti,
-                        0x3 => Opcode::Sltiu,
+                0b0010011 => match funct3 {
+                    0x0 => Opcode::Addi,
+                    0x4 => Opcode::Xori,
+                    0x6 => Opcode::Ori,
+                    0x7 => Opcode::Andi,
+                    0x1 => Opcode::Slli,
+                    0x5 => match imm {
+                        0x00 => Opcode::Srli,
+                        0x20 => Opcode::Srai,
                         _ => panic!("unknown opcode"),
-                    }
-                }
+                    },
+                    0x2 => Opcode::Slti,
+                    0x3 => Opcode::Sltiu,
+                    _ => panic!("unknown opcode"),
+                },
                 // load
                 0b0000011 => match funct3 {
                     0x0 => Opcode::Lb,
@@ -159,10 +159,11 @@ fn decode_opcode(
                     _ => panic!("unknown opcode"),
                 },
                 0b1100111 => Opcode::Jalr,
-                0b1110011 => {
-                    // not implemented requires immediate value check
-                    todo!()
-                }
+                0b1110011 => match imm {
+                    0x0 => Opcode::Ecall,
+                    0x1 => Opcode::Ebreak,
+                    _ => panic!("unknown opcode"),
+                },
                 _ => panic!("unknown opcode"),
             }
         }
