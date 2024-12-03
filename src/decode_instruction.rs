ad50dea50579ae94cfbc986490b5732bdf0888d4
Wisdom Ogwu <iammadab@gmail.com>
2024-12-03 19:08:14 +0100

    test negative immediate resolution for s type

--- src/decode_instruction.rs
+++ src/decode_instruction.rs
@@ -313,5 +313,8 @@ mod tests {
         assert_eq!(decode_instruction(0x00C58513).imm, 12);
         // sw x8, 6(x4)
         assert_eq!(decode_instruction(0x00822323).imm, 6);
+        // sw x8, -6(x4)
+        let expected: u32 = !6 + 1;
+        assert_eq!(decode_instruction(0xfe822d23).imm, expected);
     }
 }
