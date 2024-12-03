89d860a457b30b195949ca09a3af9c03dec7c14a
Wisdom Ogwu <iammadab@gmail.com>
2024-12-03 09:04:19 +0100

    add instruction type + opcode enum

--- src/decode_instruction.rs
+++ src/decode_instruction.rs
@@ -1,9 +1,74 @@
+enum InstructionType {
+    R,
+    I,
+    S,
+    B,
+    U,
+    J
+}
+
+enum Opcode {
+    Add,
+    Sub,
+    Xor,
+    Or,
+    And,
+    Sll,
+    Srl,
+    Sra,
+    Slt,
+    Sltu,
+
+    Addi,
+    Xori,
+    Ori,
+    Andi,
+    Slli,
+    Srli,
+    Srai,
+    Slti,
+    Sltiu,
+
+    Lb,
+    Lh,
+    Lw,
+    Lbu,
+    Lhu,
+
+    Sb,
+    Sh,
+    Sw,
+
+    Beq,
+    Bne,
+    Blt,
+    Bge,
+    Bltu,
+    Bgeu,
+
+    Jal,
+    Jalr,
+
+    Lui,
+    Auipc,
+
+    Ecall,
+    Ebreak,
+    Fence
+}
+
 struct DecodedInstruction {
-    opcode: u32
+    inst_type: InstructionType,
+    opcode: Opcode
 }
 
 fn decode_instruction(instruction: u32) -> DecodedInstruction {
+    let opcode_value = instruction & mask(6);
     todo!()
+}
+
+fn mask(n: u8) -> u32 {
+    (1 << n) - 1
 }
 
 #[test]
