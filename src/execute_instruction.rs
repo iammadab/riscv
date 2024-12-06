ccc7593f04d825fe395923e62dc1436f14e10ddb
Wisdom Ogwu <iammadab@gmail.com>
2024-12-06 11:15:42 +0100

    wip

--- src/execute_instruction.rs
+++ src/execute_instruction.rs
@@ -3,6 +3,7 @@ use crate::vm::VM;
 
 pub(crate) fn execute_instruction(vm: &mut VM, instruction: DecodedInstruction) {
     match instruction.opcode {
+        // R Type Instructions
         Opcode::Add => {
             *vm.reg_mut(instruction.rd) = vm.reg(instruction.rs1) + vm.reg(instruction.rs2)
         }
@@ -35,6 +36,8 @@ pub(crate) fn execute_instruction(vm: &mut VM, instruction: DecodedInstruction) 
         Opcode::Sltu => {
             unimplemented!()
         }
+
+        // I Arithmetic Instructions
         Opcode::Addi => {}
         Opcode::Xori => {}
         Opcode::Ori => {}
@@ -44,24 +47,35 @@ pub(crate) fn execute_instruction(vm: &mut VM, instruction: DecodedInstruction) 
         Opcode::Srai => {}
         Opcode::Slti => {}
         Opcode::Sltiu => {}
+
+        // I Memory Instructions
         Opcode::Lb => {}
         Opcode::Lh => {}
         Opcode::Lw => {}
         Opcode::Lbu => {}
         Opcode::Lhu => {}
+
+        // Store Instructions
         Opcode::Sb => {}
         Opcode::Sh => {}
         Opcode::Sw => {}
+
+        // Branch Instructions
         Opcode::Beq => {}
         Opcode::Bne => {}
         Opcode::Blt => {}
         Opcode::Bge => {}
         Opcode::Bltu => {}
         Opcode::Bgeu => {}
+
+        // Jump Instructions
         Opcode::Jal => {}
         Opcode::Jalr => {}
+
         Opcode::Lui => {}
         Opcode::Auipc => {}
+
+        // System Instructions
         Opcode::Ecall => {}
         Opcode::Ebreak => {}
         Opcode::Eother => {}
